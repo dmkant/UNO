b88be483bdd24e8f1a9759bbeaff1a1277d76602
dmkant <52276740+dmkant@users.noreply.github.com>
2023-04-09 22:17:42 +0200

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-SERVERURL = "http://192.168.1.5:5000/";
-// SERVERURL = "https://dmkant.pythonanywhere.com/";
+// SERVERURL = "http://192.168.1.5:5000/";
+SERVERURL = "https://dmkant.pythonanywhere.com/";
 
-DOMAINE = "https://dmkant.github.io/uno/";
+DOMAINE = "https://dmkant.github.io/UNO/";
