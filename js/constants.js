e00d659722b090cdb7e77f250078803261d8816e
dmkant <52276740+dmkant@users.noreply.github.com>
2023-04-15 13:39:51 +0200

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
-SERVERURL = "http://192.168.1.5:5000/";
-// SERVERURL = "https://dmkant.pythonanywhere.com/";
+// SERVERURL = "http://192.168.1.5:5000/";
+SERVERURL = "https://dmkant.pythonanywhere.com/";
 
-// DOMAINE = "https://dmkant.github.io/uno/";
-DOMAINE = "file:///C:/Users/kalea/Formation/Ensai/3A/RL/uno/frontend/";
+DOMAINE = "https://dmkant.github.io/UNO/";
+// DOMAINE = "file:///C:/Users/kalea/Formation/Ensai/3A/RL/uno/frontend/";
